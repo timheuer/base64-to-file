3ab47b004c4bc26b8ae9ee5327cc3f6e5d755450
Tim Heuer <tim@timheuer.com>
2019-12-14 16:26:10 -0800

    slashes are fun

--- dist/index.js
+++ dist/index.js
@@ -57,7 +57,7 @@ const core = __webpack_require__(470);
 const fs = __webpack_require__(747);
 
 // get input parameter values from config
-var fileName = process.env.RUNNER_TEMP + '\\' + core.getInput('fileName');
+var fileName = process.env.RUNNER_TEMP + '/' + core.getInput('fileName');
 var encodedString = core.getInput('encodedString');
 
 // most @actions toolkit packages have async methods
