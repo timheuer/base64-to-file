3c8c8bac833d2b82dba19f18a61cd7e2b2d19b93
Tim Heuer <tim@timheuer.com>
2019-12-14 16:17:11 -0800

    logging to see what the process.env vars are #sigh

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var encodedString = core.getInput('encodedString');
 // most @actions toolkit packages have async methods
 async function run() {
   try { 
+    console.log(process.env);
     const tempFile = Buffer.from(encodedString, 'base64');
     
     if (tempFile.length == 0)
